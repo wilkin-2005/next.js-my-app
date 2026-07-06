76b10ce48ef3c45bc20179faa1fd1b6a6d162b18
Wilmer Kindstedt <wilmer.kindstedt@gmail.com>
2026-07-06 16:32:51 +0200

    Började med min hero section.

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,65 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <main>
+
+      {/* Roberts Hero section */}
+      <section className="relative min-h-[60vh] flex flex-col justify-center items-center px-4 py-16 bg-radial-[at_50%_45%] from-violet-900/40 from-20% via-cyan-900/20 via-45% to-black to-70%">
+        <div className="border-2 border-teal-400/50 bg-teal-400/10 rounded-full overflow-hidden inline-flex items-center justify-center p-4 motion-safe:animate-bounce ">
+        <span className="material-symbols material-symbols-filled text-4xl text-teal-400">rocket</span>
+        </div>
+        <header className="relative z-10 max-w-3xl flex flex-col justify-center items-center px-4 text-center">
+          <h1 className="font-extrabold font-outfit text-zinc-200 text-6xl text-balance mb-6 leading-none">
+            Welcome to <span className="text-teal-400">Planet Express</span>{" "}
+            Academy
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="text-lg text-zinc-300 text-pretty mb-10 max-w-2xl">
+            Master the art of intergalactic delivery, advanced propulsion
+            physics, and avoiding Zapp Brannigan's tactical incompetence. Your
+            journey to the 31st century starts here.
           </p>
+          <div className="flex flex-col sm:flex-row gap-4 justify-center">
+            <a
+              className="px-8 py-4 flex gap-2 justify-center items-center uppercase bg-teal-400 text-zinc-950 font-bold rounded-xl hover:bg-cyan-400/80"
+              href="/enroll"
+            >
+              Enroll now
+               <span className="material-symbols material-symbols-outlined text-2xl">bolt</span>
+            </a>
+            <a
+              className="px-8 py-4 flex gap-2 justify-center items-center uppercase border border-cyan-400 text-cyan-400 hover:bg-cyan-400/20 font-bold rounded-xl hover:bg-cyan-400/30"
+              href="/classes"
+            >
+              View Classes
+            </a>
+          </div>
+        </header>
+        <Image
+          className="w-full h-auto object-cover z-0 opacity-40"
+          src="/space.png"
+          fill
+          alt=""
+        />
+      </section>
+
+      {/* Min Hero section */}
+      <section className="relative min-h-[60vh] flex flex-col justify-center items-center px-4 py-16">
+        <div>
+          <h1 className="font-black text-7xl text-balance mb-6">Hejsan React och Next.js!</h1>
+
+          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consectetur ab omnis, laudantium ipsum praesentium.</p>
+
+          <button type="button">Knapp 1</button>
+          <button type="button">Knapp 2</button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="grid grid-cols-3 gap-4">
+          <img src="https://picsum.photos/200/300?random=1" alt="" />
+          <img src="https://picsum.photos/200/300?random=2" alt="" />
+          <img src="https://picsum.photos/200/300?random=3" alt="" />
         </div>
-      </main>
-    </div>
+      </section>
+
+    </main>
   );
 }
